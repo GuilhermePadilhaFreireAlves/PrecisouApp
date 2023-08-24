f34dec742a6fbd92549a2283d8bb59a229dfe2c0
Guilherme <guilhermepadilhafreirealves@gmail.com>
2023-08-23 21:58:46 -0300

    scrollview,arrowfunction to class,listas nome/desc

--- src/screens/ServicoScreen.js
+++ src/screens/ServicoScreen.js
@@ -1,39 +1,108 @@
-import React from 'react'
-import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native'
 import UsuarioComponent from '../components/UsuarioComponent'
 import Barrabotoes from '../components/BarradosBotoes'
+import { Estrelas } from '../components/Estrelas'
+class ServicoScreen extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
 
-const ServicoScreen = () => {
-  return (
+      nome: [
+        'Joao Henrique',
+        'Marcos Pedro',
+        'Guilherme Pontes',
+        'Valdir Bisse',
+        'Fernando dos Santos'
+      ],
+      descricaoavaliacao: [
+        'Bom trabalho, recomendo muito, chegou no horário muito simpático',
+        'Otimo trabalho, recomendo muito, chegou no horário muito simpático',
+        'Medio trabalho, recomendo muito, chegou no horário muito simpático',
+        'Maravilhoso trabalho, recomendo muito, chegou no horário muito simpático',
+        'Mais ou menos viu, trabalho, recomendo muito, chegou no horário muito simpático'
+
+      ]
+    }
+  }
+
+  render () {
+    return (
     <SafeAreaView>
         <View style = {styles.container}>
             <UsuarioComponent/>
 
             <View style = {styles.containerDescricao}>
                 <Text style = {styles.textTituloServ}>
-                    Serviços
+                  Serviços
                 </Text>
                 <Text style = {styles.textDescricaoServ}>
                     Faço de casa, limpo os moveis, portas e janelas, adoro cachorros e gatos, mas caso sejam agressivos, por favor preendam-os. Os valores variam de caso para caso
                 </Text>
                 <Text style = {styles.textmoney}>
-                    Valor : R$ 100 - 200
+                           Valor : R$ 100 - 200
                 </Text>
             </View>
             <View style = {styles.containerAvaliações}>
                 <Text style = {styles.textAvaliacoes}>
                     Avaliações
                 </Text>
+
+                <ScrollView showsVerticalScrollIndicator = {false}>
                 <View style = {styles.containerAvaliacoestexto}>
-                    <Text style = {styles.textaAvaliacoestexto}>
-                    Faço de casa, limpo os moveis, portas e janelas, adoro cachorros e gatos, mas caso sejam agressivos, por favor preendam-os. Os valores variam de caso para caso
+                  <View >
+                    <Text style = {styles.nomeAvaliacao}>
+                      {this.state.nome[1]}
                     </Text>
+                    <Estrelas/>
+                    <Text style = {styles.descAvaliacao}>
+                      {this.state.descricaoavaliacao[1]}
+                    </Text>
+                  </View>
+                  <View >
+                    <Text style = {styles.nomeAvaliacao}>
+                      {this.state.nome[2]}
+                    </Text>
+                    <Estrelas/>
+                    <Text style = {styles.descAvaliacao}>
+                      {this.state.descricaoavaliacao[2]}
+                    </Text>
+                  </View>
+                  <View >
+                    <Text style = {styles.nomeAvaliacao}>
+                      {this.state.nome[3]}
+                    </Text>
+                    <Estrelas/>
+                    <Text style = {styles.descAvaliacao}>
+                      {this.state.descricaoavaliacao[3]}
+                    </Text>
+                  </View>
+                  <View >
+                    <Text style = {styles.nomeAvaliacao}>
+                      {this.state.nome[4]}
+                    </Text>
+                    <Estrelas/>
+                    <Text style = {styles.descAvaliacao}>
+                      {this.state.descricaoavaliacao[4]}
+                    </Text>
+                  </View>
+                  <View >
+                    <Text style = {styles.nomeAvaliacao}>
+                      {this.state.nome[5]}
+                    </Text>
+                    <Estrelas/>
+                    <Text style = {styles.descAvaliacao}>
+                      {this.state.descricaoavaliacao[5]}
+                    </Text>
+                  </View>
                 </View>
+                </ScrollView>
             </View>
             <Barrabotoes />
         </View>
     </SafeAreaView>
-  )
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -92,7 +161,7 @@ const styles = StyleSheet.create({
     marginTop: 10
 
   },
-  textaAvaliacoestexto: {
+  nomeAvaliacao: {
     fontSize: 20,
     fontFamily: 'Neucha',
     color: '#000000',
@@ -100,8 +169,15 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginRight: 17,
     textAlign: 'justify'
+  },
+  descAvaliacao: {
+    fontSize: 17,
+    fontFamily: 'Neucha',
+    color: '#000000',
+    marginLeft: 17,
+    marginRight: 17,
+    textAlign: 'justify'
   }
 
 })
-
 export default ServicoScreen
