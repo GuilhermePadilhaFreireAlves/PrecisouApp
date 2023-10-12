6d780d480b0b2d19e0b63abfce7bc2b296cd2fd5
Guilherme <guilhermepadilhafreirealves@gmail.com>
2023-10-12 11:37:26 -0300

    separei a parte antes de logar da dps de logar

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import LoginScreen from '../screens/loginScreen'
+import LoginScreen2 from '../screens/loginScreen2'
+import CadastroUsuario from '../screens/cadastroUsuario'
+const { Navigator, Screen } = createNativeStackNavigator()
+export function AuthRoutes () {
+  return (
+      <Navigator screenOptions={{ headerShown: false }}>
+            <Screen
+            name = 'login1p'
+            component={LoginScreen}
+            />
+              <Screen
+            name = 'login2p'
+            component={LoginScreen2}
+            />
+             <Screen
+            name = 'cadastroUsuariop'
+            component={CadastroUsuario}
+            />
+      </Navigator>
+  )
+}
