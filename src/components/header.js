9a2c3e0db25c0de73919ebec8adddcea246eed8e
Guilherme <guilhermepadilhafreirealves@gmail.com>
2023-10-12 11:33:46 -0300

    botao de logout

---
+++ src/components/header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Text, TouchableOpacity, StyleSheet } from 'react-native'
+import auth from '@react-native-firebase/auth'
+export function Header () {
+  function handleSignOut () {
+    auth().signOut()
+  }
+
+  return (
+
+    <TouchableOpacity onPress={handleSignOut}>
+      <Text style={styles.textInput}>
+        Logout
+      </Text>
+    </TouchableOpacity>
+  )
+}
+const styles = StyleSheet.create({
+  container: {
+
+    backgroundColor: 'ADD9FF'
+  }
+})
