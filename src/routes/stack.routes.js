49075432b52aa5c8d2f0beb6c69a4dc08819a925
Guilherme <guilhermepadilhafreirealves@gmail.com>
2023-10-17 19:23:41 -0300

    add a home screen primeiro

--- src/routes/stack.routes.js
+++ src/routes/stack.routes.js
@@ -3,19 +3,23 @@ import React from 'react'
 import ServicoScreen from '../screens/ServicoScreen'
 import ContaScreen from '../screens/contaScreen'
 import Barrabotoes from '../components/BarradosBotoes'
+import HomeScreen from '../screens/homeScreen'
 const { Screen, Navigator } = createNativeStackNavigator()
 
 export function StackRoutes () {
   return (
         <Navigator screenOptions={{ headerShown: false }}>
-
             <Screen
-            name = 'contap'
-            component={ContaScreen}
+            name = 'homep'
+            component={HomeScreen}
             />
               <Screen
             name = 'servicop'
             component={ServicoScreen}
+            />
+            <Screen
+            name = 'contap'
+            component={ContaScreen}
             />
                 <Screen
             name = 'barrabtnp'
