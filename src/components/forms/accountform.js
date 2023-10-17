5619369178d7fd900e5c5127ffd218e1392f69a0
Guilherme <guilhermepadilhafreirealves@gmail.com>
2023-10-17 19:21:37 -0300

    alerta erro + envio pro bd nome atrelado  user.id

--- src/components/forms/accountform.js
+++ src/components/forms/accountform.js
@@ -9,19 +9,41 @@ import {
 
 } from 'react-native'
 import auth from '@react-native-firebase/auth'
+import database from '@react-native-firebase/database'
 
 export function AccountForm () {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [nome, setNome] = useState('')
+  const [name, setName] = useState('')
   const [isLoading, setIsLoading] = useState(false)
 
   function handleNewAccount () {
     setIsLoading(true)
 
-    auth().createUserWithEmailAndPassword(email, password, nome)
-      .then(() => Alert.alert('Conta', 'cadastrada com sucesso!'))
-      .catch((error) => console.log(error))
+    auth().createUserWithEmailAndPassword(email, password)
+      .then((value) => {
+        // Alert.alert(value.user.uid)
+        database().ref('usuarios').child(value.user.uid).set({
+          nome: name
+        })
+      })
+      .catch(error => {
+        console.log(error)
+        if (error.code === 'auth/weak-password') {
+          Alert.alert('Senha fraca', 'Sua senha precisa conter no minimo seis caracteres')
+          // eslint-disable-next-line no-useless-return
+          return
+        }
+        if (error.code === 'auth/email-already-in-use') {
+          Alert.alert('Email já cadastrado', 'Este email já foi cadastrado, caso necessario vá em login para redefinir a senha')
+          // eslint-disable-next-line no-useless-return
+          return
+        } else {
+          Alert.alert('ERRO', 'Senha ou email invalido')
+          // eslint-disable-next-line no-useless-return
+          return
+        }
+      })
       .finally(() => setIsLoading(false))
   }
   return (
@@ -31,7 +53,8 @@ export function AccountForm () {
           style={styles.input}
           placeholder="Nome:"
           underlineColorAndroid="rgb(79, 172, 254)"
-          onChangeText={setNome}
+          onChangeText={(name) => setName(name)}
+          value={name}
         />
     <TextInput
           style={styles.inputsenha}
