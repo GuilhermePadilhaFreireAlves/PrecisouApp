476188c7520b6bbf604f3ad14da7265d19ac259c
Guilherme <guilhermepadilhafreirealves@gmail.com>
2023-10-30 12:47:48 -0300

    arrumei, agora o nome atualiza jano login/cadastro

--- src/contexts/nomeContext.js
+++ src/contexts/nomeContext.js
@@ -1,30 +1,34 @@
-import React, { useState, createContext, useEffect } from 'react'
+import React, { useState, createContext, useEffect, useContext } from 'react'
 import database from '@react-native-firebase/database'
 import PropTypes from 'prop-types'
 import auth from '@react-native-firebase/auth'
 
 export const NomeContext = createContext({})
 
+export function useNome () {
+  return useContext(NomeContext)
+}
+
 function NomeProvider ({ children }) {
   const [nomepa, setNomepa] = useState('carregando...')
 
   useEffect(() => {
-    async function Nomedados () {
-      const user = auth().currentUser
+    const unsubscribe = auth().onAuthStateChanged((user) => {
       if (user) {
-        database().ref(`usuarios/${user.uid}/nome`).once('value', (snapshot) => {
-          setNomepa(snapshot.val())
-        })
+        database()
+          .ref(`usuarios/${user.uid}/nome`)
+          .on('value', (snapshot) => {
+            setNomepa(snapshot.val())
+          })
+      } else {
+        setNomepa('Não autenticado')
       }
-    }
-    Nomedados()
+    })
+
+    return () => unsubscribe()
   }, [])
 
-  return (
-    <NomeContext.Provider value={{ nomepa }}>
-      {children}
-    </NomeContext.Provider>
-  )
+  return <NomeContext.Provider value={{ nomepa }}>{children}</NomeContext.Provider>
 }
 
 NomeProvider.propTypes = {
