aa30e7bf88d97d630a6cf38d8f5c82282f802613
Guilherme <guilhermepadilhafreirealves@gmail.com>
2023-10-30 10:25:27 -0300

    visual base da tela de cadastro prestador

---
+++ src/screens/cadastroPrestador.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
+import PropTypes from 'prop-types'
+
+import { Accountprestadorform } from '../components/forms/accountprestadorform'
+const Cadastroprestador = () => {
+  return (
+    <ScrollView>
+    <View style = {styles.container}>
+
+      <Image
+      source = {require('../../assets/imagens/logo.png')} style = {styles.imageLayout}
+      />
+      <Text style = {styles.textotitulo}>Adicione novas informações</Text>
+        <Accountprestadorform/>
+    </View>
+    </ScrollView>
+
+  )
+}
+Cadastroprestador.propTypes = {
+  navigation: PropTypes.object.isRequired
+}
+const styles = StyleSheet.create({
+  containerKeyboard: {
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#ffffff'
+  },
+  imageLayout: {
+    marginTop: 25,
+    height: 250,
+    width: 300
+  },
+  textotitulo: {
+    fontSize: 30,
+    color: '#4FACFE',
+    marginTop: 50,
+    fontFamily: 'Neucha'
+
+  },
+
+  botaocadastro: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 50
+
+  },
+  buttonTextLog: {
+    fontSize: 20,
+    color: '#289AFF',
+    fontFamily: 'Neucha'
+
+  }
+
+})
+
+export default Cadastroprestador
